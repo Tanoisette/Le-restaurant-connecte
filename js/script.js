87198c382080d4fab759885065d7f7e05afa7780
Tanoisette <raimi1411@icloud.com>
2026-03-20 14:07:54 +0100

    ajout des menus et amelioration de la page

--- js/script.js
+++ js/script.js
@@ -5,3 +5,19 @@
   toggle.addEventListener("click", () => {
     nav.classList.toggle("active");
   });
+
+  function showPage(page) {
+  const page1 = document.querySelector(".page1");
+  const page2 = document.querySelector(".page2");
+
+  if (page === 1) {
+    page1.style.display = "block";
+    page2.style.display = "none";
+  } else {
+    page1.style.display = "none";
+    page2.style.display = "block";
+  }
+}
+
+// page 1 affichée par défaut
+window.onload = () => showPage(1);
