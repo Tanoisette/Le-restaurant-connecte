45d2cb83542e4c6d63fc047c6948cbfbbb60bcaa
Tanoisette <raimi1411@icloud.com>
2026-03-20 15:31:00 +0100

    docs: ajout de content dans readme

--- README.md
+++ README.md
@@ -16,3 +16,9 @@ fais pour un projet de rattrapage pour les cours
 
 ## Auteur
  Raïmi radji 1INFO à EFFICOM
+
+ ## probleme et perspective d'ameliorations 
+
+j'ai réussi a a repondre a casi tout les critéres juste un probleme au niveau de la responsive sur la page galerie est reservations ,le burger buton ne nmarche pas a cahaque fois il y a aussi un petit bug au niveau du caroussel il va trop vite je n'ai pas encore pu le coirrigé
+
+ perspective d'amélioration ajoiut d'un vrai formulaire avec du nodejs ou un form externe qui envoi un vrai mail pour la réservation , j'avais commencé a faire des recherches mais quand j'ai vu que le'utulisation nu nodejs et fortement recommandcé j'ai pas pu aboutir jusque au bout 
--- js/script.js
+++ js/script.js
@@ -1,21 +1,24 @@
-
+document.addEventListener("DOMContentLoaded", () => {
   const toggle = document.getElementById("menu-toggle");
   const nav = document.getElementById("nav");
 
-  toggle.addEventListener("click", () => {
-    nav.classList.toggle("active");
-  });
+  if (toggle && nav) {
+    toggle.addEventListener("click", () => {
+      nav.classList.toggle("active");
+    });
+  }
+});
+function showPage(page) {
+const page1 = document.querySelector(".page1");
+const page2 = document.querySelector(".page2");
 
-  function showPage(page) {
-  const page1 = document.querySelector(".page1");
-  const page2 = document.querySelector(".page2");
-
-  if (page === 1) {
+if (page === 1) {
     page1.style.display = "block";
     page2.style.display = "none";
-  } else {
+} else {
     page1.style.display = "none";
     page2.style.display = "block";
   }
 }
 
+console.log("menu loaded");
